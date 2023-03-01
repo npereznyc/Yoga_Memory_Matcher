58c297a9551af087f5ef27818e96fca03d2038dd
npereznyc <natalieperez84@gmail.com>
2023-03-01 14:20:42 -0600

    Timer incrementing by seconds

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,26 @@
+import React from "react"
+import { useState } from "react"
+
 function Nav() {
+    const [seconds, setSeconds] = useState(0)
+    const [time, setTime] = useState(0)
+    const [start, setStart] = useState(false)
+
+    const startTimer = () =>{
+        setInterval(()=>{
+            setSeconds(seconds => seconds + 1)
+        }, 1000)
+
+        document.querySelector('.start-btn').setAttribute('disabled', 'true')
+    }
+
+    const currentCount = seconds
 
     return (
         <>
-        <button>Start</button>
-        
+        <button className='start-btn' onClick={startTimer}>Start</button>
+        <h4>Time: <span id='counter'>{currentCount}</span></h4>
+        <h4>Score: <span>0</span></h4>
         </>
     )
 }
