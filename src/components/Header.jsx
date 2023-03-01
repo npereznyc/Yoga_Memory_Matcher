68564927fc928df4c420230cc60b7d692604e7ba
npereznyc <natalieperez84@gmail.com>
2023-03-01 13:46:09 -0600

    Header component created. Nav component started.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1>Name That Pose!</h1>
+            <h4>Match the yoga poses to their names. You have 2 minutes to match them all. Click the start button to get started</h4>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+
+    return (
+        <>
+        <button>Start</button>
+        
+        </>
+    )
+}
+
+export default Nav
